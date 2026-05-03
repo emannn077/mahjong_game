9a195b2f30b7a01c7bb14a8462e59d1e9bd53a8d
emannn077 <emanrauf779@gmail.com>
2026-05-03 16:53:34 +0300

    implemented GameContext for managing game state

---
+++ src/context/GameContext.jsx
@@ -0,0 +1,21 @@
+import {createContext, useContext, useState} from "react";
+
+const GameContext = createContext();
+export const GameProvider= ({children})=>{
+  const [drawPile, setDrawPile]=useState([]);
+  const [discardPile,setDiscardPile]=useState([]);
+  const[currentHand,setcurrentHand]=useState([]);
+  const[tileValue,setTileValues]=useState([]);
+  const [score, setScore]=useState(0);
+  const [reshuffles, setReshuffles]=useState(0);
+  const [gameOver, setGameOver]= useState(false);
+
+  return (
+    <GameContext.Provider value={{
+      drawPile,setDrawPile,discardPile,setDiscardPile,currentHand,setcurrentHand,tileValue,setTileValues,score,setTileValues,reshuffles,setReshuffles,gameOver,setGameOver
+    }}>
+      {children}
+      </GameContext.Provider>
+  )
+}
+
